b5c08a897ba80006761aa04c9ea1074655373060
Kostiantyn <71460426+KosStr@users.noreply.github.com>
2020-11-28 16:13:47 +0200

    Add files via upload

---
+++ AddTask.xaml.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="AddTask.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TaskAssistant
+{
+    using System;
+    using System.Linq;
+    using System.Windows;
+    using DBTaskAssistant;
+    using DBTaskAssistant.ViewModels;
+
+    /// <summary>
+    /// Class that adds logic to Add Task View.
+    /// </summary>
+    public partial class AddTask : Window
+    {
+        AddVM addVM;
+        TaskAssistantContext taskADB;
+        User currentUser = new User();
+
+        public AddTask(User loggedUser)
+        {
+            this.InitializeComponent();
+            this.taskADB = new TaskAssistantContext();
+            currentUser = loggedUser;
+            addVM = (AddVM)DataContext;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            TaskModel newTask = new TaskModel();
+            newTask.Note = Node.Text;
+            newTask.Date = new DateTime(addVM.date.Year, addVM.date.Month, addVM.date.Day, addVM.currHour, addVM.currMinute, 1);
+            newTask.Priority = Convert.ToInt32(addVM.priority);
+
+            // Generate different values!!!
+            newTask.Id = rnd.Next((taskADB.Tasks.Count() + 1) * 2);
+            Task task = new Task(newTask.Id, currentUser.Username, newTask.Note, newTask.Date, newTask.Priority);
+            currentUser.Tasks.Add(task);
+            taskADB.Users.Find(currentUser.Username).Tasks.Add(task);
+            taskADB.Tasks.Add(task);
+            taskADB.SaveChanges();
+            this.Close();
+        }
+    }
+}
